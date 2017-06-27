a3fccbc645cf701ba66f82d59044c45a8f85002d
Thiago Hissa <thiagohissa@gmail.com>
2017-06-27 17:58:00 -0400

    first one

--- Threelow/main.m
+++ Threelow/main.m
@@ -9,9 +9,6 @@
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
+
     return 0;
 }
