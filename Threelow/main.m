530d47be979515d4206f5bb6e9a56d622e3d4d49
Thiago Hissa <thiagohissa@gmail.com>
2017-06-27 17:54:35 -0400

    first commit

---
+++ Threelow/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Threelow
+//
+//  Created by Thiago Hissa on 2017-06-27.
+//  Copyright © 2017 Thiago Hissa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
